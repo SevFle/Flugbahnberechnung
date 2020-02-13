46f668f80c19dd46aedae7539143d4b5675dc06b
Severin Fleckenstein <Severin_Sebastian.Fleckenstein@rfh-campus.de>
2020-02-13 18:45:36 +0100

    Restructuring

--- Flugbahnberechnung/OpenCV_unmanaged.cpp
+++ Flugbahnberechnung/OpenCV_unmanaged.cpp
@@ -1,37 +1,49 @@
-#pragma unmanaged
 #pragma once
-
+/**** Includes ****************************************************************/
+#pragma region Includes
 #include "OpenCV_unmanaged.h"
 
+/**** Include OpenCV ****************************************************************/
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+/**** Include TBB *******************************************************************/
+#include <tbb/concurrent_queue.h>
+#include <tbb/pipeline.h>
+#include <tbb/tbb.h>
+
+#pragma endregion
+
+/**** Namespaces***************************************************************/
 using namespace nmsp_open_cv_unmanaged;
 
 
-
-C_opencv_unmanaged::C_opencv_unmanaged()
+c_opencv_unmanaged::c_opencv_unmanaged()
   {
   }
 
-C_opencv_unmanaged::~C_opencv_unmanaged()
+c_opencv_unmanaged::~c_opencv_unmanaged()
   {
   }
 
 
-bool C_opencv_unmanaged::apply_filter (cv::Mat& cpu_src, cv::Mat& cpu_dst)
+bool c_opencv_unmanaged::apply_filter (cv::Mat& cpu_src, cv::Mat& cpu_dst)
   {
   return false;
   }
 
-bool C_opencv_unmanaged::create_camera_vector ()
+bool c_opencv_unmanaged::create_camera_vector ()
   {
-  for (int i =0; i<= this->Cameras_in_use; i++)
+  for (int i =0; i<= this->cameras_in_use; i++)
     {
     camera_vector.push_back(camera_capture(i, capture_api_));
+    return false;
     }
+  return false;
   }
   
-bool C_opencv_unmanaged::tbb_camera_pipeline (size_t ntoken)
+bool c_opencv_unmanaged::tbb_camera_pipeline (size_t ntoken)
   {
-  tbb::parallel_pipeline
+  /*tbb::parallel_pipeline
     (
     ntoken,
     tbb::make_filter<void, cv::Mat>
@@ -40,7 +52,7 @@ bool C_opencv_unmanaged::tbb_camera_pipeline (size_t ntoken)
         {
         grabbed_frame = camera_vector[current_pipeline_camera_grab].grab;
         current_pipeline_camera_grab++;
-        if (current_pipeline_camera_grab > Cameras_in_use-1)
+        if (current_pipeline_camera_grab > cameras_in_use-1)
           {
           current_pipeline_camera_grab = 0;
           }
@@ -55,90 +67,102 @@ bool C_opencv_unmanaged::tbb_camera_pipeline (size_t ntoken)
         }
       )
       
-    )
+    )*/
+  
   }
 
-bool C_opencv_unmanaged::create_camera_threads_objectcalibration()
+bool c_opencv_unmanaged::create_camera_threads_objectcalibration()
   {
   for (int i=0; i<6; i++)
     {
-    create_camera_thread;
+    //create_camera_thread;
     }
   return false;
   }
 
-void  thread_camera_grabbing (int cam_ID)
-  {
-  while (true)
-    {
-
-    }
-  }
-
-bool state_machine()
+bool c_opencv_unmanaged::state_machine()
   {
   switch (statemachine_state)
     {
       case 1:
-      
+
+        statemachine_state = 2;
+
       case 2:
+
+        statemachine_state = 3;
+
       case 3:
+
+        statemachine_state = 4;
+
+      case 4:
+
+        statemachine_state = 5;
+
+      case 5:
+
+        statemachine_state = 6;
+
+      case 6:
+
+        statemachine_state = 1;
+
     }
+  return false;
   }
 
 
-bool C_opencv_unmanaged::grab_frame(cv::Mat& cpu_dst)
+bool c_opencv_unmanaged::grab_frame(cv::Mat& cpu_dst)
   {
 
   return false;
   }
 
-void C_opencv_unmanaged::push_to_gpu(cv::Mat& cpu_src, cv::cuda::GpuMat& gpu_dst)
-  {
-  gpu_dst.upload(cpu_src);
-  }
-
-void C_opencv_unmanaged::pull_from_gpu(cv::cuda::GpuMat& gpu_src, cv::Mat& cpu_dst)
-  {
-  //cpu_dst.download(gpu_src);
-  }
-
-void C_opencv_unmanaged::to_hsv(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst)
+void c_opencv_unmanaged::push_to_gpu(cv::Mat& cpu_src, cv::cuda::GpuMat& gpu_dst)
   {
   }
 
-void C_opencv_unmanaged::to_hsv_threshold(cv::cuda::GpuMat& img, cv::cuda::GpuMat& gpu_dst, int& hue_min, int& hue_max, int& saturation_min, int& saturation_max, int& value_min, int& value_max)
+void c_opencv_unmanaged::pull_from_gpu(cv::cuda::GpuMat& gpu_src, cv::Mat& cpu_dst)
   {
   }
 
-void C_opencv_unmanaged::to_resize(cv::Mat& cpu_src, cv::Mat& cpu_dst, int& size_x, int& size_y)
+void c_opencv_unmanaged::to_hsv(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst)
   {
   }
 
-void C_opencv_unmanaged::erosion(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& erosion_kernel_size, int& erosion_iterations)
+void c_opencv_unmanaged::to_hsv_threshold(cv::cuda::GpuMat& img, cv::cuda::GpuMat& gpu_dst, int& hue_min, int& hue_max, int& saturation_min, int& saturation_max, int& value_min, int& value_max)
   {
   }
 
-void C_opencv_unmanaged::dilation(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& dilation_kernel_size, int& dilation_iterations)
+void c_opencv_unmanaged::to_resize(cv::Mat& cpu_src, cv::Mat& cpu_dst, int& size_x, int& size_y)
   {
   }
 
-void C_opencv_unmanaged::opening(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& opening_kernel_size)
+void c_opencv_unmanaged::erosion(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& erosion_kernel_size, int& erosion_iterations)
   {
   }
 
-void C_opencv_unmanaged::closing(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& closing_kernel_size)
+void c_opencv_unmanaged::dilation(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& dilation_kernel_size, int& dilation_iterations)
   {
   }
 
-void C_opencv_unmanaged::bilateral(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& bilateral_kernel_size, float& bilateral_sigma_color, float& bilateral_sigma_space)
+void c_opencv_unmanaged::opening(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& opening_kernel_size)
   {
   }
 
-void C_opencv_unmanaged::gaussian_bluring(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& gaussian_kernel_size, double& gaussian_sigma)
+void c_opencv_unmanaged::closing(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& closing_kernel_size)
   {
   }
 
-void C_opencv_unmanaged::find_contours(cv::Mat & cpu_src, cv::Mat & cpu_dst)
+void c_opencv_unmanaged::bilateral(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& bilateral_kernel_size, float& bilateral_sigma_color, float& bilateral_sigma_space)
+  {
+  }
+
+void c_opencv_unmanaged::gaussian_bluring(cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& gaussian_kernel_size, double& gaussian_sigma)
+  {
+  }
+
+void c_opencv_unmanaged::find_contours(cv::Mat & cpu_src, cv::Mat & cpu_dst)
   {
   }
--- Flugbahnberechnung/OpenCV_unmanaged.h
+++ Flugbahnberechnung/OpenCV_unmanaged.h
@@ -1,124 +1,166 @@
-#pragma unmanaged
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
-
-#include <msclr/marshal_cppstd.h>
-
-#include <tbb/concurrent_queue.h>
-#include <tbb/pipeline.h>
-#include <tbb/tbb.h>
+#pragma once
 
 #include <vector>
+#include <opencv2/core/cuda.hpp>
+
+/**** Vorwärtsdeklaration der unmanaged OpenCV Klassen ***************************/
+namespace cv
+  {
+  class VideoCapture;
+  class Mat;
+    namespace cuda
+    {
+      GpuMat;
+    }using namespace cuda;
+  } using namespace cv;
+/**** Vorwärtsdeklaration der unmanaged TBB Klassen ******************************/
+namespace tbb
+  {
+  class pipeline;
+  }using namespace tbb;
+
+
 
 namespace nmsp_open_cv_unmanaged
   {
-  class C_opencv_unmanaged
+  class c_opencv_unmanaged
     {
-      public:
-      C_opencv_unmanaged();
-
-      ~C_opencv_unmanaged();
-
-
-
-      struct ProcessingChainData
-        {
-        cv::Mat img;
-        std::vector<cv::Rect> faces, faces2;
-        cv::Mat gray, smallImg;
-        };
       
-      int                   camera_id;
-      int                   hue_min;
-      int                   hue_max;
-      int                   saturation_min;
-      int                   saturation_max;
-      int                   value_min;
-      int                   value_max;
-
-      int                   target_size_x;     //Resizing the image to the wanted Values
-      int                   target_size_y;
-
-
-      int                   erosion_kernel_size;
-      int                   erosion_iterations;
-      int                   dilation_kernel_size;
-      int                   dilation_iterations;
-
-      int                   bilateral_kernel_size;
-      float                 bilateral_sigma_color;
-      float                 bilateral_sigma_space;
-      int                   gaussian_kernel_size;
-      double                gaussian_sigma;
-      int                   opening_kernel_size;
-      int                   closing_kernel_size;
-      int                   morph_kernel_size;
-      int                   Cameras_in_use;
-      int                   current_pipeline_camera_grab;
-      int                   statemachine_state;
-
-      cv::Mat                original_frame;
-      cv::Mat                processed_frame;
-      cv::Mat               grabbed_frame;
-
-      volatile bool         done;
-
-    private:
-      cv::Mat                hsv_frame;
-      cv::Mat                gaussian_frame;
-      cv::Mat                bilateral_frame;
-
-      cv::cuda::GpuMat       gpu_original_frame;
-      cv::cuda::GpuMat       gpu_processed_frame;
-      cv::cuda::GpuMat       gpu_HSV_frame;
-      cv::cuda::GpuMat       gpu_Gaussian_frame;
-      cv::cuda::GpuMat       gpu_Bilateral_frame;
-      cv::cuda::GpuMat       gpu_src_img;
-      cv::cuda::GpuMat       gpu_temp_img;
-      cv::cuda::GpuMat       gpu_dst_img;
-
-      cv::VideoCapture       camera_capture;
-      std::vector<cv::VideoCapture> camera_vector;
-      std::vector<std::vector<cv::Mat>> camera_captured_vector;
-
-      int                    camera_vector_count;
-      int                    capture_api_;
-
-
-
-      tbb::pipeline pipeline;
-
-
       public:
-      bool  apply_filter          (cv::Mat& cpu_src, cv::Mat& cpu_dst);
-      bool start_camera_thread    ();
-      bool create_camera_vector   ();
-      bool create_camera_threads_objectcalibration();
-      bool state_machine          ();
+/**** Konstruktor ****************************************************************/
+      c_opencv_unmanaged ();
+/**** Dekonstruktor **************************************************************/
+      ~c_opencv_unmanaged ();
+/**** Öffentliche Variablen  *****************************************************/
+      #pragma region Öffentliche Variablen
+
+      int                                     cameras_in_use;
+      int                                     current_pipeline_camera_grab;
+
+      volatile bool                           done;
+      #pragma endregion 
+/**** Öffentliche Klassenvariablen  **********************************************/
+      int                                     camera_id;
+/**** Öffentliche OpenCV-Variablen  **********************************************/
+      #pragma region Öfffentliche OpenCV-Variablen
+      int                                     hue_min,
+                                              hue_max,
+                                              saturation_min,
+                                              saturation_max,
+                                              value_min,
+                                              value_max;
+
+       int                                    target_size_x,     //Resizing the image to the wanted Values
+                                              target_size_y;
+
+
+      int                                     erosion_kernel_size,
+                                              erosion_iterations,
+                                              dilation_kernel_size,
+                                              dilation_iterations;
+
+      int                                     bilateral_kernel_size,
+                                              gaussian_kernel_size,
+                                              opening_kernel_size,
+                                              closing_kernel_size,
+                                              morph_kernel_size;
+
+      double                                  gaussian_sigma;
+
+
+      float                                   bilateral_sigma_color,
+                                              bilateral_sigma_space;
+
+      #pragma endregion
+/**** Öffentliche OpenCV-Objekte  ************************************************/
+      #pragma region  Öffentliche OpenCV-Objekte
+
+      cv::Mat^                                original_frame,
+                                              processed_frame,
+                                              grabbed_frame;
+
+      #pragma endregion
 
 
       private:
-      bool  grab_frame            (cv::Mat& cpu_dst);
-      void  push_to_gpu           (cv::Mat& src, cv::cuda::GpuMat& gpu_dst);
-      void  pull_from_gpu         (cv::cuda::GpuMat& gpu_src, cv::Mat& dst);
+/**** Private OpenCV-Objekte  ****************************************************/
+      #pragma region  Private OpenCV-Objekte
+
+      cv::VideoCapture^                       camera_capture;
+       
+      cv::Mat^                                hsv_frame,
+                                              gaussian_frame,
+                                              bilateral_frame;
+
+      cv::cuda::GpuMat^                       gpu_original_frame,
+                                              gpu_processed_frame,
+                                              gpu_HSV_frame,
+                                              gpu_Gaussian_frame,
+                                              gpu_Bilateral_frame,
+                                              gpu_src_img,
+                                              gpu_temp_img,
+                                              gpu_dst_img;
+      #pragma endregion 
+/**** Private OpenCV-Variablen  **************************************************/
+      #pragma region Private OpenCV-Variablen
+
+      #pragma endregion
+/**** Private Variablen  *********************************************************/
+      #pragma region Variablen
+
+      std::vector<cv::VideoCapture>           camera_vector;
+      std::vector<std::vector<cv::Mat>>       camera_captured_vector;
+
+      int                                     camera_vector_count;
+      int                                     capture_api_;
+      int                                     statemachine_state;
+      #pragma endregion
+/**** Private TBB-Objekte ********************************************************/
+      #pragma region TBB-Objekte
+
+      tbb::pipeline^                          pipeline;
+
+      #pragma endregion
 
 
-      void  to_hsv                (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst);
-      void  to_hsv_threshold      (cv::cuda::GpuMat& img, cv::cuda::GpuMat& gpu_dst, int& hue_min, int& hue_max, int& saturation_min, int& saturation_max, int& value_min, int& value_max);
-      void  to_resize             (cv::Mat& cpu_src, cv::Mat& cpu_dst, int& size_x, int& size_y);
-      void  erosion               (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& erosion_kernel_size, int& erosion_iterations);
-      void  dilation              (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& dilation_kernel_size, int& dilation_iterations);
 
-      void  opening               (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& opening_kernel_size);
-      void  closing               (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& closing_kernel_size);
-      void  bilateral             (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& bilateral_kernel_size, float& bilateral_sigma_color, float& bilateral_sigma_space);
-      void  gaussian_bluring      (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& gaussian_kernel_size, double& gaussian_sigma);
+/**** Öffentliche Klassenmethoden*************************************************/
+      #pragma region Öffentliche Klassenmethoden
+      public:
 
-      void  find_contours         (cv::Mat& cpu_src, cv::Mat& cpu_dst);
+      bool            apply_filter                                    (cv::Mat& cpu_src, cv::Mat& cpu_dst);
+      bool            start_camera_thread                             ();
+      bool            create_camera_vector                            ();
+      bool            tbb_camera_pipeline                             (size_t ntoken);
+      bool            create_camera_threads_objectcalibration         ();
+
+      #pragma endregion
+
+/**** Private Klassenmethoden*****************************************************/
+      #pragma region Private Klassenmethoden
+      private:
+
+      bool            grab_frame                                      (cv::Mat& cpu_dst);
+      bool            state_machine                                   ();
 
 
+      void            push_to_gpu                                     (cv::Mat& src, cv::cuda::GpuMat& gpu_dst);
+      void            pull_from_gpu                                   (cv::cuda::GpuMat& gpu_src, cv::Mat& dst);
+      #pragma region Filtermethoden
+      void            to_hsv                                          (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst);
+      void            to_hsv_threshold                                (cv::cuda::GpuMat& img, cv::cuda::GpuMat& gpu_dst, int& hue_min, int& hue_max, int& saturation_min, int& saturation_max, int& value_min, int& value_max);
+      void            to_resize                                       (cv::Mat& cpu_src, cv::Mat& cpu_dst, int& size_x, int& size_y);
+      void            erosion                                         (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& erosion_kernel_size, int& erosion_iterations);
+      void            dilation                                        (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& dilation_kernel_size, int& dilation_iterations);
 
-    };
+      void            opening                                         (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& opening_kernel_size);
+      void            closing                                         (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& closing_kernel_size);
+      void            bilateral                                       (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& bilateral_kernel_size, float& bilateral_sigma_color, float& bilateral_sigma_space);
+      void            gaussian_bluring                                (cv::cuda::GpuMat& gpu_src, cv::cuda::GpuMat& gpu_dst, int& gaussian_kernel_size, double& gaussian_sigma);
 
-  }
+      void            find_contours                                   (cv::Mat& cpu_src, cv::Mat& cpu_dst);
+      #pragma endregion
+      #pragma endregion
+    };// c_opencv_unmanaged
+  }//nmsp_open_cv_unmanaged
 
