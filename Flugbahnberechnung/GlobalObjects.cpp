69e287c55828720051df052359a139087e6cc4cb
Severin Fleckenstein <Severin_Sebastian.Fleckenstein@rfh-campus.de>
2020-03-15 20:56:32 +0100

    Added Save2CSV

--- Flugbahnberechnung/GlobalObjects.cpp
+++ Flugbahnberechnung/GlobalObjects.cpp
@@ -9,9 +9,11 @@ C_GlobalObjects::C_GlobalObjects()
   {
   cameras_in_use = 0;
   camera_id = 0;
+  this->csv_parameter_datei = new C_CSV_Parameter_Datei();
   }
 C_GlobalObjects::~C_GlobalObjects()
   {
+  delete (this->csv_parameter_datei);
   camera_id = 0;
   cameras_in_use = 0;
   }
--- Flugbahnberechnung/GlobalObjects.h
+++ Flugbahnberechnung/GlobalObjects.h
@@ -1,13 +1,17 @@
 #pragma once
+#include "CSV_Datei.h"
+
+using namespace UM_CSV_Datei;
 /*********************************************************** Definition Namespace **************************************************************/
 namespace nmsp_GlobalObjects
   {
-  public ref class C_GlobalObjects
+   class C_GlobalObjects
     {
       public:
       C_GlobalObjects();
       ~C_GlobalObjects();
 
+      C_CSV_Parameter_Datei* csv_parameter_datei;
 
       int cameras_in_use;
       int camera_id;
