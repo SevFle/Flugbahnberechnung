e9c005816fa92e02691c70ceeeb14dbbe9c8a399
Severin Fleckenstein <Severin_Sebastian.Fleckenstein@rfh-campus.de>
2020-04-12 12:32:29 +0200

    Minor changes

--- Flugbahnberechnung/opencv_unmanaged.h
+++ Flugbahnberechnung/opencv_unmanaged.h
@@ -20,7 +20,6 @@ namespace nmsp_opencv_unmanaged
       cv::cuda::GpuMat*                        gpu_src_img;
       cv::cuda::GpuMat*                        gpu_thresholded;
       cv::cuda::GpuMat*                        gpu_filtered;
-      cv::cuda::GpuMat*                        gpu_gray;
 
 
       //HSV-Filtering
@@ -33,20 +32,18 @@ namespace nmsp_opencv_unmanaged
       /**************************************************** Öffentliche Klassenobjekte ********************************************************/
       public:
       cv::VideoCapture*                        cap;
-      cv::Mat*                                 cpu_temp;
-      cv::Mat*                                 cpu_temp3;
       cv::Mat*                                 cpu_src_img;
-      cv::Mat*                                 cpu_mid_img;
       cv::Mat*                                 cpu_masked_img;
       cv::Mat*                                 cpu_filtered;
       cv::Mat*                                 cpu_contoured;
       cv::Mat*                                 cpu_undistorted;
+
       cv::Mat*                                 DistCoeffs;
       cv::Mat*                                 Intrinsic;
+      cv::Mat*                                 map1;
+      cv::Mat*                                 map2;
 
 
-
-      //std::vector<nmsp_opencv_unmanaged::c_opencv_unmanaged*>          camera_vector;
       /**************************************************** Öffentliche Anwender-Attribute ********************************************************/
       public:
       int                                     camera_id;
@@ -70,17 +67,12 @@ namespace nmsp_opencv_unmanaged
       /*********************************************************** Öffentliche OpenCV-Variablen  **************************************************/
       #pragma region Öfffentliche OpenCV-Variablen
       public:
-      uchar                                     hue_min;
-      uchar                                     hue_max;
-      uchar                                     saturation_min;
-      uchar                                     saturation_max;
-      uchar                                     value_min;
-      uchar                                     value_max;
-
-
-      int                                     target_size_width;     //Resizing the image to the wanted width Values
-      int                                     target_size_height;     //Resizing the image to the wanted height Values
-
+      uchar                                   hue_min;
+      uchar                                   hue_max;
+      uchar                                   saturation_min;
+      uchar                                   saturation_max;
+      uchar                                   value_min;
+      uchar                                   value_max;
 
       int                                     erosion_iterations;
       int                                     dilation_iterations;
@@ -95,66 +87,50 @@ namespace nmsp_opencv_unmanaged
       int                                     closing_kernel_size;
       int                                     morph_kernel_size;
       int                                     gaussian_kernel_size;
-      int                                     ksize;
-      int                                     objekt_anzahl;
-
-      int                                     bordertype;
-
-      int                                     numBoards;
-      int                                     numCornersHor;
-      int                                     numcornersVer;
 
       double                                  gaussian_sigma;
 
-      double                                  Vec_Object[3];
-      double                                  max_Moment_m00;
 
 
       float                                   bilateral_sigma_color;
       float                                   bilateral_sigma_spatial;
 
-      std::vector<std::vector<cv::Point>> contours;
-      std::vector<cv::Vec4i> hirarchy;
 
-      bool  erode_active;
-      bool  dilate_active;
-      bool  open_active;
-      bool  close_active;
-      bool  gaussian_active;
-      bool  morph_active;
+      bool                                    erode_active;
+      bool                                    dilate_active;
+      bool                                    gaussian_active;
+      bool                                    morph_active;
 
-      bool  contour_found;
-      bool  idle;
+      bool                                    idle;
 
 
-
-      cv::Point                               anchor;
 
 
       #pragma endregion
 
       /********************************************************* Öffentliche Klassenmethoden*******************************************************/
       public:
-      void            apply_filter                                        (cv::Mat*         cpu_src, cv::Mat *cpu_dst);
-      void            cpu_mask_img                                        (cv::Mat*         hsv_cpu_src, cv::Mat* cpu_masked_dst);
 
       void            camera_thread                                       ();
-      void            set_calibration_parameter                           (double           (&DistCoeffs)[1][5], double              (&Intrinsic)[3][3]);
-      void            get_calibration_parameter                           (double           (&DistCoeffs)[1][5], double              (&Intrinsic)[3][3]);
+
+
+      void            set_calibration_parameter                           (double           (&DistCoeffs)[1][5], double  (&Intrinsic)[3][3]);
+      void            get_calibration_parameter                           (double           (&DistCoeffs)[1][5], double  (&Intrinsic)[3][3]) const;
       void            set_aspect_ratio                                    (int              Height,   int width);
       void            set_framerate                                       (int              framerate);
+
+
       /******************************************************* Private Klassenmethoden***************************************************************/
       private:
       void            init                                                (int              camera_id);
       void            cpu_grab_frame                                      (cv::Mat*         cpu_dst_img);
+      void            apply_filter                                        (cv::Mat*         cpu_src, cv::Mat *cpu_dst);
 
-      void            gpu_to_hsv_threshold                                (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
-      void            gpu_inRange                                         (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst, cv::Scalar minvalue, cv::Scalar maxvalue);
 
-      void            gpu_erode                                           (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst, int borderType, cv::Point anchor);
-      void            gpu_dilate                                          (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst, cv::Point anchor);
-      void            gpu_open                                            (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst, cv::Point anchor);
-      void            gpu_close                                           (cv::cuda::GpuMat* gpu_src,cv::cuda::GpuMat* gpu_dst, cv::Point anchor);
+      void            gpu_erode                                           (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst, int borderType);
+      void            gpu_dilate                                          (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
+      void            gpu_open                                            (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
+      void            gpu_close                                           (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
 
       void            gpu_bilateral_filter                                (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
       void            gpu_gaussian_filter                                 (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
@@ -164,7 +140,8 @@ namespace nmsp_opencv_unmanaged
       void            gpu_filter_bgr                                      (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
       void            gpu_filter_gray                                     (cv::cuda::GpuMat* gpu_src, cv::cuda::GpuMat* gpu_dst);
 
-      void            find_contours                                       (cv::Mat* thresholded_source_image, cv::Mat* dst_contoured_image); 
+      void            find_contours                                       (cv::Mat* thresholded_source_image, cv::Mat* dst_contoured_image);
+
       void            undistord_img                                       (cv::Mat* cpu_src, cv::Mat* cpu_dst);
 
 
